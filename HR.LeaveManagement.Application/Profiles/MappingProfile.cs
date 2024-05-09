96a46dde446e94aa9402d929c87a80e93dbcc9f7
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-05-09 14:52:45 +0600

    test

--- HR.LeaveManagement.Application/Profiles/MappingProfile.cs
+++ HR.LeaveManagement.Application/Profiles/MappingProfile.cs
@@ -4,11 +4,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Domain;
 
 namespace HR.LeaveManagement.Application.Profiles
 {
     public class MappingProfile: Profile
     {
-
+        public MappingProfile()
+        {
+            CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            
+        }
     }
 }
