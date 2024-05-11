52a8636ace3e1489b2ca863b8b0e4069f17d1d35
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-05-11 23:41:23 +0600

    m

--- HR.LeaveManagement.Application/Features/LeaveType/Handlers/Queries/GetLeaveTypeListRequestHandlers.cs
+++ HR.LeaveManagement.Application/Features/LeaveType/Handlers/Queries/GetLeaveTypeListRequestHandlers.cs
@@ -1,5 +1,7 @@
-﻿using HR.LeaveManagement.Application.DTOs;
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
 using HR.LeaveManagement.Application.Features.LeaveType.Requests;
+using HR.LeaveManagement.Application.Persistence.Contracts;
 using MediatR;
 using System;
 using System.Collections.Generic;
@@ -11,9 +13,17 @@ namespace HR.LeaveManagement.Application.Features.LeaveType.Handlers.Queries
 {
     public class GetLeaveTypeListRequestHandlers : IRequestHandler<GetLeaveTypeListRequest, List<LeaveTypetDto>>
     {
-        public Task<List<LeaveTypetDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        private readonly ILeaveTypeRepository _leaveTypeRepository;
+        private readonly IMapper _mapper;
+        public GetLeaveTypeListRequestHandlers(ILeaveTypeRepository leaveTypeRepository, IMapper mapper) 
         {
-            throw new NotImplementedException();
+            this._leaveTypeRepository = leaveTypeRepository;
+            this._mapper = mapper;
+        }
+        public async Task<List<LeaveTypetDto>> Handle(GetLeaveTypeListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveType = await _leaveTypeRepository.GetAll();
+            return _mapper.Map<List<LeaveTypetDto>>(leaveType);
         }
     }
 }
