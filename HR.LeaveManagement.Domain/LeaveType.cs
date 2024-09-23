f91f354951d87555e5616a46af1de674efe49163
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-09-23 12:05:58 +0600

    delete old code

--- HR.LeaveManagement.Domain/LeaveType.cs
+++ HR.LeaveManagement.Domain/LeaveType.cs
@@ -9,7 +9,7 @@ namespace HR.LeaveManagement.Domain
 {
     public class LeaveType : BaseDomainEntity
     {
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
         public int DefaultDays { get; set; }
     }
 }
