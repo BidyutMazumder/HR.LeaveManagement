6ea86409a422ab5a684ee7b23ae9b4e9f0aaf265
Bidyut mazumder <46864109+BidyutMazumder@users.noreply.github.com>
2024-10-01 14:46:45 +0600

    a

---
+++ HR.LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Persistence
+{
+    public static class PersistenceServiceRegistration
+    {
+        public static IServiceCollection AddPersistenceService(this IServiceCollection services, string name)
+        {
+            return services;
+        }
+    }
+}
